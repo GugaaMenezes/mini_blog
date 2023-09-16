d01be0cc8c7192196c10f191dc1d16b82c7942c9
Gustavo Menezes <gustavo.menezes@live.com>
2023-09-16 20:08:08 -0300

    feat: endpoint for generate token and update requirements.txt

--- mini_blog/mini_blog/urls.py
+++ mini_blog/mini_blog/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from miniblog.views import generate_token
+
 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
@@ -8,5 +10,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('miniblog.urls')),
     path('token/', TokenObtainPairView.as_view()),
-    path('token/refresh/', TokenRefreshView.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view()),    
+    path('', generate_token, name='generate_token'),
 ]
--- mini_blog/miniblog/views.py
+++ mini_blog/miniblog/views.py
@@ -2,3 +2,29 @@ from django.shortcuts import render
 
 def insert_article(request):
     return render(request, 'insert_article.html')
+
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+
+@csrf_exempt
+def generate_token(request):
+    
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            
+            access_token = data.get('accessToken')
+                        
+            response_data = {
+                'access_token': access_token,
+                'message': 'Token gerado com sucesso',
+            }
+            
+            return JsonResponse(response_data, status=200)
+
+        except Exception as e:
+            error_message = str(e)
+            return JsonResponse({'error': error_message}, status=401)
+
+    return render(request, 'generate_token.html')
