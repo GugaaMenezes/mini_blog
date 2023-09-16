25c17fd6ae679ee817e5d2befdfa7763ed1e4c86
Gustavo Menezes <gustavo.menezes@live.com>
2023-09-15 21:38:59 -0300

    config: Included in setting.py is the rest_framework app

--- mini_blog/mini_blog/settings.py
+++ mini_blog/mini_blog/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'miniblog'
+    'rest_framework',
+    'miniblog',
 ]
 
 MIDDLEWARE = [
