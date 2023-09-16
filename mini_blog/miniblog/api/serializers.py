841abe070673b6adbcb83b7818cf84bccadad3e8
Gustavo Menezes <gustavo.menezes@live.com>
2023-09-15 21:52:53 -0300

    feat: Created in serializers.py Keyword and Article, with reference between Keyword and Article

---
+++ mini_blog/miniblog/api/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from ..models import Article, Keyword
+
+class KeywordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Keyword
+        fields = '__all__'
+        
+class ArticleSerializer(serializers.ModelSerializer):
+    keyword_set = KeywordSerializer(many=True)
+    
+    class Meta:
+        model = Article
+        fields = '__all__'
+        
+    def create(self, validated_data):
+        keyword_data = validated_data.pop('keyword_set', [])
+        article = Article.objects.create(**validated_data)
+        
+        for keyword_info in keyword_data:
+            keyword = Keyword.objects.get_or_create(name=keyword_info['name'])
+            article.keyword_set.add(keyword)
+            
+        return article
