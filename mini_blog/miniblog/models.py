81919e683559e3935efdb2870e3558d2406b313d
Gustavo Menezes <gustavo.menezes@live.com>
2023-09-15 21:49:06 -0300

    feat: Create Keywords and Article in models.py

--- mini_blog/miniblog/models.py
+++ mini_blog/miniblog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Keyword(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    subtitle = models.CharField(max_length=255)
+    type = models.IntegerField()
+    content = models.TextField()
+    status = models.IntegerField()
+    keyword_set = models.ManyToManyField(Keyword)
+
+    def __str__(self):
+        return self.title
