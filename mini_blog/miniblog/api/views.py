77419a8a89ea598dc873b77ccea288bdac4ea2f4
Gustavo Menezes <gustavo.menezes@live.com>
2023-09-15 22:17:05 -0300

    refactor: Changed class name in views,py, Serializers for ViewSet

--- mini_blog/miniblog/api/views.py
+++ mini_blog/miniblog/api/views.py
@@ -3,10 +3,10 @@ from ..models import Article, Keyword
 from .serializers import ArticleSerializer, KeywordSerializer
 
 
-class ArticleSerializer(viewsets.ModelViewSet):
+class ArticleViewSet(viewsets.ModelViewSet):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
     
-class KeywordSerializer(viewsets.ModelViewSet):
+class KeywordViewSet(viewsets.ModelViewSet):
     queryset = Keyword.objects.all()
     serializer_class = KeywordSerializer
